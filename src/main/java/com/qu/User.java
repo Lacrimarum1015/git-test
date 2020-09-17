4ae4a9e6f0c8c84d49850f0526b4ad05fa6ce472
Lacrimarum1015 <747681480@qq.com>
2020-09-18 00:00:54 +0800

    1

---
+++ src/main/java/com/qu/User.java
@@ -0,0 +1,12 @@
+package com.qu;
+
+/**
+ * @title: User
+ * @description: TODO
+ * @author：quLiangquan
+ * @date 2020/9/17 23:55
+ **/
+public class User {
+    private String name;
+    private String age;
+}
